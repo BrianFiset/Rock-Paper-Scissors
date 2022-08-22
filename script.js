cddb7234d050a0e5e12193b716023928f6ced138
BrianFiset <fisetbrian@gmail.com>
2022-08-22 18:04:27 -0400

    Add rock paper scissors 5 turn game

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Create function that has 3 coices in it and it return one of them at random
 // create function where player decide rock papper scissor and competes againt the computer.ALso return who won
 
-
+let playerScore = 0
+let computerScore = 0
 function getComputerChoice() {
     let choice = ["rock", "paper", "scissors"]
     let item = choice[Math.floor(Math.random()*choice.length)]
@@ -11,22 +12,40 @@ function getComputerChoice() {
 function playRound(computerSelection, playerSelection) {
     playerSelection = playerSelection.toLocaleLowerCase()
     if (computerSelection === "rock" && playerSelection === "rock") {
-    return "You tie! Rock ties with Rock"
+        playerScore += 0
+        computerScore += 0
+        return "You tie! Rock ties with Rock"
     } else if (computerSelection === "paper" && playerSelection === "rock") {
-    return "You Lose! Paper beats Rock"
+        playerScore += 0
+        computerScore += 1
+        return "You Lose! Paper beats Rock"
     } else if (computerSelection === "scissors" && playerSelection === "rock") {
+        playerScore += 1
+        computerScore += 0
         return "You Win! scissors loses to Rock"
     } else if (computerSelection === "paper" && playerSelection === "paper") {
+        playerScore += 0
+        computerScore += 0
         return "You tie! Paper ties with Paper"
     } else if (computerSelection === "scissors" && playerSelection === "paper") {
+        playerScore += 0
+        computerScore += 1
         return "You Lose! scissors beats paper"
     } else if (computerSelection === "rock" && playerSelection === "paper") {
+        playerScore += 1
+        computerScore += 0
         return "You Win! rock loses to paper"
     } else if (computerSelection === "paper" && playerSelection === "scissors") {
+        playerScore += 1
+        computerScore += 0
         return "You Win! Paper loses to scissors"
     } else if (computerSelection === "scissors" && playerSelection === "scissors") {
+        playerScore += 0
+        computerScore += 0
         return "You tie! scissors ties scissors"
     } else if (computerSelection === "rock" && playerSelection === "scissors") {
+        playerScore += 0
+        computerScore += 1
         return "You Lose! rock beats scissors"
     } else {
         return "Invalid Text"
@@ -34,5 +53,20 @@ function playRound(computerSelection, playerSelection) {
 }
 
 const playerSelection = "SciSsors"
-const computerSelection = getComputerChoice()
-console.log(playRound(computerSelection, playerSelection))
+
+function game() {
+    for (let i = 0; i < 5 ; i++) {
+        console.log(playRound(getComputerChoice(), prompt("Rock Paper Scissors")))
+    }
+    console.log("Player: " + playerScore)
+    console.log("Computer: " + computerScore)
+    if(playerScore > computerScore){
+        console.log("You Win")
+    } else if(playerScore < computerScore) {
+        console.log("You lose")
+    } else {
+        console.log("You tied")
+    }
+}
+
+game()
